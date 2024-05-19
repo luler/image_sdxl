ef09cb2e1b74055af6ee48f816afa0000586bbbf
我只想看看蓝天 <1207032539@qq.com>
2024-05-19 18:04:28 +0800

    更换为cloudflare的ai服务

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import retrying
 from PIL import Image
 
 
-@retrying.retry(stop_max_attempt_number=5, wait_fixed=1000)
+@retrying.retry(stop_max_attempt_number=3, wait_fixed=1000)
 def translate(text):
     with requests.get('https://lingva.thedaviddelta.com/api/v1/auto/en/' + text) as response:
         text = ''
@@ -19,48 +19,30 @@ def translate(text):
         return text
 
 
-@retrying.retry(stop_max_attempt_number=10, wait_fixed=1000)
-def get_image_url(text):
+def get_image_content(text, guidance):
     headers = {
         "Content-Type": "application/json",
-        'Authorization': 'Bearer ' + os.getenv('MYSTICAI_API_KEY'),
+        'Authorization': 'Bearer ' + os.getenv('CLOUDFLARE_AI_TOKEN'),
     }
     data = {
-        "pipeline": "ainzoil/sd-xl:v15",
-        "inputs": [
-            {
-                "type": "string",
-                "value": text
-            },
-            {
-                "type": "dictionary",
-                "value": {
-                    "batch": 1,
-                    "denoising_end": 0.8,
-                    "guidance_scale": 7,
-                    "height": 1024,
-                    "negative_prompt": " ",
-                    "num_inference_steps": 50,
-                    "seed": 0,
-                    "width": 1024
-                }
-            }
-        ]
+        "guidance": guidance,
+        "num_steps": 20,
+        "prompt": text,
+        "strength": 1
     }
-    with requests.post('https://www.mystic.ai/v4/runs', data=json.dumps(data), headers=headers) as response:
-        res = response.json()
-        return res['outputs'][0]['value'][0]['file']['url']
+    with requests.post('https://api.cloudflare.com/client/v4/accounts/' + os.getenv(
+            'CLOUDFLARE_AI_ACCOUNT_ID') + '/ai/run/@cf/stabilityai/stable-diffusion-xl-base-1.0', data=json.dumps(data),
+                       headers=headers) as response:
+        return response.content
 
 
-def sdxl(text):
-    image_url = get_image_url(text)
-    with requests.get(image_url) as response_image:
-        img = Image.open(io.BytesIO(response_image.content))
-        return img
+@retrying.retry(stop_max_attempt_number=3, wait_fixed=1000)
+def sdxl(text, guidance):
+    return Image.open(io.BytesIO(get_image_content(text, guidance)))
 
 
-def dosomething(text):
-    return sdxl(translate(text))
+def dosomething(text, guidance):
+    return sdxl(translate(text), guidance)
 
 
 # 这里是主程序的代码
@@ -69,8 +51,9 @@ if __name__ == "__main__":
     dotenv.load_dotenv()
 
     ii = gr.Textbox(label='图片描述')
+    iii = gr.Slider(label='文本描述的遵循程度', minimum=1, maximum=20, value=7.5, step=0.1)
     oo = gr.Image(label='生成的图片')
 
-    demo = gr.Interface(dosomething, inputs=ii, outputs=oo, title='基于Stable Diffusion模型的图片生成')
+    demo = gr.Interface(dosomething, inputs=[ii, iii], outputs=oo, title='基于Stable Diffusion模型的图片生成')
 
-    demo.launch(server_name='0.0.0.0', server_port=7860)
+    demo.launch(server_port=7860)
